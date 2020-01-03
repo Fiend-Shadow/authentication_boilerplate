b4f50b9030b119e7ae085daceaef802f651eb36c
James Perkins <james.r.perkins@tuta.io>
2020-01-02 20:18:28 -0500

    Removing company name as required

--- validations/user.validation.js
+++ validations/user.validation.js
@@ -8,8 +8,7 @@ module.exports = {
     body: {
       email: Joi.string().email().required(),
       password: Joi.string().min(6).max(128).required(),
-      fullName: Joi.string().max(128).required(),
-      companyName: Joi.string().required()
+      fullName: Joi.string().max(128).required()
     }
   }
 }
